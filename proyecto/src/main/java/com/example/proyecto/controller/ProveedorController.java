a90a5621b3ea626b8dcec624aa724739fd259461
9irala <megaflare0009@gmail.com>
2026-09-14 23:56:03 -0500

    V3.0
    Se agrego nueva entidad y funciona sus CRUD

---
+++ proyecto/src/main/java/com/example/proyecto/controller/ProveedorController.java
@@ -0,0 +1,48 @@
+package com.example.proyecto.controller;
+
+import com.example.proyecto.entities.Proveedor;
+import com.example.proyecto.service.ProveedorService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/proveedores")
+public class ProveedorController {
+
+     private final ProveedorService proveedorService;
+
+    public ProveedorController(ProveedorService proveedorService) {
+        this.proveedorService = proveedorService;
+    }
+
+    @GetMapping
+    public List<Proveedor> listarTodos() {
+        return proveedorService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Proveedor> buscarPorId(@PathVariable Long id) {
+        return proveedorService.buscarPorId(id);
+    }
+
+    @PostMapping
+    public Proveedor guardar(@RequestBody Proveedor proveedor) {
+        return proveedorService.guardar(proveedor);
+    }
+
+    @PutMapping("/{id}")
+    public Proveedor actualizar(
+            @PathVariable Long id,
+            @RequestBody Proveedor proveedor) {
+
+        return proveedorService.actualizar(id, proveedor);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id) {
+        proveedorService.eliminar(id);
+    }
+
+}
---
+++ proyecto/src/main/java/com/example/proyecto/entities/Proveedor.java
@@ -0,0 +1,61 @@
+package com.example.proyecto.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "proveedores")
+public class Proveedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String contacto;
+
+    private String telefono;
+
+    public Proveedor() {
+    }
+
+    public Proveedor(Long id, String nombre, String contacto, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.contacto = contacto;
+        this.telefono = telefono;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+}
